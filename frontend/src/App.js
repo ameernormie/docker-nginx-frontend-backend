e3eb829e40c4646081a09eca3d113b2622f5de13
Ameer Hamza <ameernormie@gmail.com>
2021-02-24 01:04:12 +0500

    refactor: cleanup frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost/api")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data ", data);
+        setData(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data.map((item, index) => {
+          return <p key={index}>{item.name}</p>;
+        })}
       </header>
     </div>
   );
